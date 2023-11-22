1b206ddd01b9a9fc741798781d4b994007b488d6
Kavya Santhakumar <kavysan@Kavysan-MacBook-Pro-2.local>
2023-11-22 10:29:10 -0500

    changes

--- config.py
+++ config.py
@@ -15,5 +15,6 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Kavya'
     SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     
     
